2b6ccd3e09d52d6af510a8d5faa11251fc6f5f77
Nelly Dylan <308813800@qq.com>
2024-02-02 00:20:50 +0800

    限定url

--- scripts/jrslive.js
+++ scripts/jrslive.js
@@ -1,9 +1,9 @@
 let body = $response.body;
 
 
-console.log('$task:',Object.keys($task))
-console.log('$request:',Object.keys($request))
-console.log('$response:',Object.keys($response))
+console.log('$task:', JSON.stringify(Object.keys($task)))
+console.log('$request:' + JSON.stringify(Object.keys($request)))
+console.log('$response:' + JSON.stringify(Object.keys($response)))
 
 body = body.replace('disp_prompt();', '')
 $done({body})
