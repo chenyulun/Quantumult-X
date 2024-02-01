56166093aee03973c45cac7b5e0e79073bd2466c
Nelly Dylan <308813800@qq.com>
2024-02-02 00:18:04 +0800

    测试脚本

--- scripts/jrslive.js
+++ scripts/jrslive.js
@@ -1,4 +1,9 @@
-var body = $response.body;
+let body = $response.body;
+
+
+console.log('$task:',Object.keys($task))
+console.log('$request:',Object.keys($request))
+console.log('$response:',Object.keys($response))
 
 body = body.replace('disp_prompt();', '')
 $done({body})
