f782b671fe517d86dbd9eebd59cd56d5d801a217
Nelly Dylan <308813800@qq.com>
2024-02-02 01:08:18 +0800

    提交测试

--- scripts/jrslive.js
+++ scripts/jrslive.js
@@ -5,5 +5,6 @@ let body = $response.body;
 // console.log('$request:' + JSON.stringify(Object.keys($request)))
 // console.log('$response:' + JSON.stringify(Object.keys($response)))
 
-body = body.replace('disp_prompt();', '').replace(/class="item ok_kqt type1/, ($0) => `style="display:none" ${$0}`)
+body = body.replace('disp_prompt();', '').replace(/class="item ok_kqt type1/g, ($0) => `style="display:none" ${$0}`)
+
 $done({body})
