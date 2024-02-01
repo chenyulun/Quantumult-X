22720b5903f198407c10096087555291d69c878a
Nelly Dylan <308813800@qq.com>
2024-02-02 01:04:55 +0800

    提交测试

--- scripts/jrslive-sport.js
+++ scripts/jrslive-sport.js
@@ -1,4 +1,8 @@
 var body = $response.body;
 
 body = body.replace('disp_prompt();', '')
+const hasILove = body.match(/item\sok\sme[^>]+><em\s[^>]+><\/em><strong>(中文)?高清(直播)?/)
+if(hasILove) {
+   body = body.replace('item play ok me', 'item ok me').replace(/item\sok\sme(?=[^>]+><em\s[^>]+><\/em><strong>(中文)?高清(直播)?)/, 'item play ok me')
+}
 $done({body})
--- scripts/jrslive.js
+++ scripts/jrslive.js
@@ -1,9 +1,9 @@
 let body = $response.body;
 
 
-console.log('$task:', JSON.stringify(Object.keys($task)))
-console.log('$request:' + JSON.stringify(Object.keys($request)))
-console.log('$response:' + JSON.stringify(Object.keys($response)))
+// console.log('$task:', JSON.stringify(Object.keys($task)))
+// console.log('$request:' + JSON.stringify(Object.keys($request)))
+// console.log('$response:' + JSON.stringify(Object.keys($response)))
 
-body = body.replace('disp_prompt();', '')
+body = body.replace('disp_prompt();', '').replace(/class="item ok_kqt type1/, ($0) => `style="display:none" ${$0}`)
 $done({body})
