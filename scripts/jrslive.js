01b8348a42cbf9e2ac8d5c93854a1dd7a858d8d4
Nelly Dylan <308813800@qq.com>
2024-10-26 20:26:56 +0800

    fix(): 修复弹出屏蔽问题

--- scripts/jrslive.js
+++ scripts/jrslive.js
@@ -4,7 +4,7 @@ let body = $response.body;
 // console.log('$request:' + JSON.stringify(Object.keys($request)))
 // console.log('$response:' + JSON.stringify(Object.keys($response)))
 
-body = body.replace('disp_prompt();', '').replace(/class="item ok_kqt type1/g, ($0) => `style="display:none" ${$0}`);
+body = body.replace('checkAndShowPopup();', '').replace(/class="item ok_kqt type1/g, ($0) => `style="display:none" ${$0}`);
 body = body.replace(/class="web_nav_menu"/, ($0) => `style="display:none" ${$0}`);
 body = body.replace(/class="sub_time_sepa"/g, ($0) => `style="display:none" ${$0}`);
 
